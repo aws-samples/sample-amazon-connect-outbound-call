f1f33b430b0d5731214f608dd3c5bad2e8694ddf
Budi Boentaran <budiboen@amazon.com>
2024-06-19 10:13:55 +0800

    Repolinter Scan

--- web-app/src/components/login/loginComp.tsx
+++ web-app/src/components/login/loginComp.tsx
@@ -1,58 +1,50 @@
-import { Text, useTheme, View, Image, Heading } from '@aws-amplify/ui-react';
-import paceLogo from '../../assets/pace-grey.png';
+import { Text, useTheme, View, Image, Heading } from "@aws-amplify/ui-react";
+import paceLogo from "../../assets/pace-grey.png";
 
 export const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image alt="FSI PACE" src={paceLogo} padding={tokens.space.medium} />
+        <Text
+          variation="info"
+          as="strong"
+          lineHeight="1.5em"
+          fontWeight={400}
+          fontSize="2em"
+          fontStyle="normal"
+          textDecoration="none"
+          width="30vw"
+        >
+          Amazon Connect Outbound Call
+        </Text>
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+
+  SignIn: {
     Header() {
       const { tokens } = useTheme();
-  
+
       return (
-        <View textAlign="center" padding={tokens.space.large}>
-          <Image
-            alt="PACE Connect Prototype"
-            src={paceLogo}
-            padding={tokens.space.medium}
-          />
-          <Text
-              variation="info"
-              as="strong"
-              lineHeight="1.5em"
-              fontWeight={400}
-              fontSize="2em"
-              fontStyle="normal"
-              textDecoration="none"
-              width="30vw"
-            >
-              PACE Connect Prototype
-          </Text>
-        </View>
-  
-  
-      );
-    },
-  
-    Footer() {
-      const { tokens } = useTheme();
-  
-      return (
-        <View textAlign="center" padding={tokens.space.large}>
-          <Text color={tokens.colors.neutral[80]}>
-            &copy; All Rights Reserved
-          </Text>
-        </View>
-      );
-    },
-  
-    SignIn: {
-      Header() {
-        const { tokens } = useTheme();
-  
-        return (
-          <Heading level={3} padding={`${tokens.space.xl} ${tokens.space.xl} 0`}>
+        <Heading level={3} padding={`${tokens.space.xl} ${tokens.space.xl} 0`}>
           Sign in to your Account
         </Heading>
-        );
-      },
-    }
-  
-  }
-  
+      );
+    },
+  },
+};
